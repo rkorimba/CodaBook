528f90ff073bb99708f344ccd34e2451939d30da
rkorimba <korimbacus.riad@gmail.com>
2018-05-31 13:02:30 +0200

    Authentification avec Firebase

--- CodaBook/Controller/ConnexionController.swift
+++ CodaBook/Controller/ConnexionController.swift
@@ -53,12 +53,30 @@ class ConnexionController: UIViewController {
         view.endEditing(true)
     }
     
+    func completion(_ user: AuthDataResult?, _ error: Error?) {
+        
+        if let erreur = error {
+            let nsErreur = erreur as NSError
+            if nsErreur.code == 17011 {
+                //Creer utilisateur
+                Auth.auth().createUser(withEmail: mailTF.text!, password: mdpTF.text!, completion: completion(_:_:))
+            } else {
+                Alerte.montrer.erreur(message: nsErreur.convertirErreurFirebaseEnString(), controller: self)
+            }
+        }
+        if let utilisateur = user {
+            // Verifier si l'utilisateur est dans la base de donnees>
+            Alerte.montrer.erreur(message: "Utilisateur créé dans auth", controller: self)
+        }
+        
+    }
+    
     @IBAction func seConnecterAction(_ sender: Any) {
         
         self.view.endEditing(true)
         if let adresse = mailTF.text, adresse != "" {
             if let mdp = mdpTF.text, mdp != "" {
-                
+                Auth.auth().signIn(withEmail: adresse, password: mdp, completion: completion(_: _:))
             } else {
                 Alerte.montrer.erreur(message: MDP_VIDE, controller: self)
             }
